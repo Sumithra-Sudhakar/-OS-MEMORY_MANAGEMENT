526aa74a7db76078c5be7cfb6f89640cfc920324
Mfawa <bossmanma@gmail.com>
2022-05-23 20:41:44 +0100

    revamped code

--- main.py
+++ main.py
@@ -1,84 +1,123 @@
-block_size=[]
-process_size=[]
-p_num=int(input("number of processes"))
-b_num=int(input("number of blocks"))
-print("enter the memory requirement for each process")
-for i in range(p_num):
-  a=int(input())
-  process_size.append(a)
-print("enter the block size for each block")
-for i in range(b_num):
-  a=int(input())
-  block_size.append(a)
+out = open("output.txt", "w")
 
-def first_fit():
-  for i in process_size:
-    for j in block_size:
-      if i<j:
-     
-        print("process",end=" ")
-        print(process_size.index(i)+1,end=" ")
-        print("is allocated to block",end=" ")
-        print(block_size.index(j)+1)
-        j-=i
-        break
-def best_fit():
- 
-  index=0
-  found=0
-  for i in process_size:
-    d=1000
-    for j in block_size:
-      if i<=j:
-        found=1
-        diff=j-i
-        if diff <d:
-          d=diff
-          index = block_size.index(j)
-        
-    if found==1:  
-      
-      print("process",end=" ")
-      print(process_size.index(i)+1,end=" ")
-      print("is allocated to block",end=" ")
-      print(index+1)
-      block_size[index]-=i
-    
-def worst_fit():
-  index='x'
-  found=0
-  for i in process_size:
-     
-      m=max(block_size)
-      if m>=i:
-        index = block_size.index(m)
-        found=1
-        print("process",end=" ")
-        print(process_size.index(i)+1,end=" ")
-        print("is allocated to block",end=" ")
-        print(index+1)
-        block_size.remove(m)
-      
-      if found==0:
-        print("process",end=" ")
-        print(process_size.index(i)+1,end=" ")
-        print("is allocated to block",end=" ")
-        print(index)
+def first_fit(mem,pro):
+    global out
+    out.write("First-Fit Memory Allocation\n\n")
+    out.write("------------------------------------\n\n")
+    out.write("start =>")
+    for i in mem:
+        out.write(" "+str(i))
+    out.write("\n\n")
+    for i in range(0,len(pro)):
+        kontrol=False
+        for j in range(0,len(mem)):
+            if(mem[j]>pro[i]):
+                kontrol=True
+                mem.append(-1)
+                mem[j+1:len(mem)]=mem[j:len(mem)-1]
+                mem[j+1]=mem[j]-pro[i]
+                mem[j]=-1*pro[i]
+                out.write(str(pro[i])+" =>")
+                for i in mem:
+                    if i < 0:
+                        out.write(" "+str((-1*i))+"*")
+                    elif i==0:
+                        continue
+                    else:
+                        out.write(" "+str(i))
+                out.write("\n\n")
+                break
+        if kontrol==False:
+            out.write(str(pro[i])+" => not allocated, must wait")
+            out.write("\n\n")
 
-        
-        
-print("enter the option ")
-print("1. first fit ")
-print("2. best fit")
-print("3. worst fit")
-opt=int(input())
-if opt==1:
-  first_fit()
-elif opt==2:
-  best_fit()
-elif opt==3:
-  worst_fit()
-else:
-  print("invalid option")
 
- 
+def best_fit(mem,pro):
+    global out
+    out.write("Best-Fit Memory Allocation\n\n")
+    out.write("------------------------------------\n\n")
+    out.write("start =>")
+    for i in mem:
+        out.write(" "+str(i))
+    out.write("\n\n")
+    for i in range(0,len(pro)):
+        fark=100000
+        index=-1
+        for j in range(0,len(mem)):
+            islem=mem[j]-pro[i]
+            if islem >= 0 and islem<fark:
+                fark=islem
+                index=j
+        if index==-1:
+            out.write(str(pro[i])+" => not allocated, must wait")
+            out.write("\n\n")
+            continue
+        mem.append(-1)
+        mem[index+1:len(mem)]=mem[index:len(mem)-1]
+        mem[index+1]=mem[index]-pro[i]
+        mem[index]=-1*pro[i]
+        out.write(str(pro[i])+" =>")
+        for i in mem:
+            if i < 0:
+                out.write(" "+str((-1*i))+"*")
+            elif i ==0:
+                continue
+            else:
+                out.write(" "+str(i))
+        out.write("\n\n") 
+def worst_fit(mem,pro):
+    global out
+    out.write("Worst-Fit Memory Allocation\n\n")
+    out.write("------------------------------------\n\n")
+    out.write("start =>")
+    for i in mem:
+        out.write(" "+str(i))
+    out.write("\n\n")
+    for i in range(0,len(pro)):
+        fark=0
+        index=-1
+        for j in range(0,len(mem)):
+            islem=mem[j]-pro[i]
+            if islem >= 0 and islem>fark:
+                fark=islem
+                index=j
+        if index==-1:
+            out.write(str(pro[i])+" => not allocated, must wait")
+            out.write("\n\n")
+            continue
+        mem.append(-1)
+        mem[index+1:len(mem)]=mem[index:len(mem)-1]
+        mem[index+1]=mem[index]-pro[i]
+        mem[index]=-1*pro[i]
+        out.write(str(pro[i])+" =>")
+        for i in mem:
+            if i < 0:
+                out.write(" "+str((-1*i))+"*")
+            elif i ==0:
+                continue
+            else:
+                out.write(" "+str(i))
+        out.write("\n\n") 
+f = open("input.txt", "r")
+memory=f.readline()
+process=f.readline()
+f.close()
+memory=memory.replace("\n","")
+process=process.replace("\n","")
+print(memory)
+print(process)
+memories=memory.split(",")
+processes=process.split(",")
+print(memories)
+print(processes)
+for i in range(0,len(memories)):
+    memories[i]=int(memories[i])
+for i in range(0,len(processes)):
+    processes[i]=int(processes[i])
+orjmem=[]
+orjmem[:]=memories
+first_fit(memories,processes)
+memories[:]=orjmem
+best_fit(memories,processes)
+memories[:]=orjmem
+worst_fit(memories,processes)
